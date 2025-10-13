6813dfc61e5e23e43c7da3e9cb64e9c882aeb763
Nataliya <antropova.n13n@gmail.com>
2025-10-13 17:17:17 +0700

    форма регистрации

--- modules/renderLogin.js
+++ modules/renderLogin.js
@@ -3,24 +3,31 @@ export const renderLoginForm = () => {
 
   const loginHtml = `
   <form class="login-form">
-        <input
-          type="text"
-          class="login-form__login"
-          id="name-input"
-          placeholder="Введите ваш логин"
-        />
-        <input 
-          type="password"
-          class="login-form__password"
-          placeholder="Введите ваш пароль"
-          id="text-input"
-        ></input>
-        <div class="login-form__button-box">
-          <button class="login-form__button">Войти</button>
-        </div>
-      </form>
+    <h1>Форма входа</h1>
+    <input
+      type="text"
+      class="login-form__login"
+      placeholder="Введите ваш логин"
+      id="name-input"
+      required
+    />
+    <input 
+      type="password"
+      class="login-form__password"
+      placeholder="Введите ваш пароль"
+      id="text-input"
+      required
+    ></input>
+    <div class="login-form__button-box">
+      <button class="login-form__button">Войти</button>
+    </div>
+  </form>
    `;
   container.innerHTML = loginHtml;
-  // сделать клик по кнопке войти, вызвать ф-ю логин из апи в нее передать то что нах-ся в полях после этого обработать ответ от сервера апи и сохранить токен и имя пользователя. Сделать переменную имя и вызвать функцию сет неим обработка ошибок, если ввел неправ логин или пароль
+  // сделать клик по кнопке войти, вызвать ф-ю логин из апи
+  // в нее передать то что нах-ся в полях после этого обработать
+  // ответ от сервера апи и сохранить токен и имя пользователя.
+  // Сделать переменную имя и вызвать функцию сет неим обработка ошибок,
+  //  если ввел неправ логин или пароль
   // если все успешно то вызвать функцию рендер комментс
 };
